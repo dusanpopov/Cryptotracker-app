d98497466badbc4c7e381aaf070320cd73dcaf7a
dusanpopov <aristocut90@gmail.com>
2020-10-15 22:31:37 +0200

    Updates

---
+++ precache-manifest.b43c20d8876b1c30335189128af4332a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad41791a23001c8a3a68cb99c027a186",
+    "url": "/Cryptotracker-app/index.html"
+  },
+  {
+    "revision": "032b2d77647e09c5bb75",
+    "url": "/Cryptotracker-app/static/css/main.c0276807.chunk.css"
+  },
+  {
+    "revision": "d07b8b8bea120ab37b47",
+    "url": "/Cryptotracker-app/static/js/2.3f9f098a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cryptotracker-app/static/js/2.3f9f098a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032b2d77647e09c5bb75",
+    "url": "/Cryptotracker-app/static/js/main.36ee8860.chunk.js"
+  },
+  {
+    "revision": "a22ff77ea5de67362a40",
+    "url": "/Cryptotracker-app/static/js/runtime-main.4b7bc0ad.js"
+  }
+]);
