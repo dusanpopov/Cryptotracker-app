9005461cced093f6879c01ed173cc034b6bbcd24
Dušan Popov <aristocut90@gmail.com>
2022-01-13 13:40:15 +0100

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -13,7 +13,6 @@ function Coin({name,image,symbol,price,volume,priceChange,marketcap}) {
         <div className='coin-data'>
           <p className='coin-price'>${price}</p>
           <p className='coin-volume'>${volume.toLocaleString()}</p>
-
           {priceChange < 0 ? (
             <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
           ) : (
